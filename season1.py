017e18b2b17b335d44f55500a2724be861e9e9e7
course-exercises <129099700+course-exercises@users.noreply.github.com>
2023-07-17 13:12:29 +0330

    Add files via upload
    first commit

---
+++ season1.py
@@ -0,0 +1,59 @@
+# exercise 1
+import math
+
+print("my name is Mohammad", "my Email address is : " , "sites.me56@gmail.com")
+
+# exercise 2
+name = input("please enter your name : ")
+print("hello", name)
+
+# exercise 3
+width = float(input("please enter width of your room : "))
+length = float(input("please enter length of your room : "))
+print("Area of your room is : ",width*length)
+
+
+# exercise 4
+CONST = 43560
+width = float(input("please enter width of your room in feet : "))
+length = float(input("please enter length of your room in feet : "))
+print("Area of your room is : ",width*length/CONST)
+
+
+# exercise 5
+
+
+
+
+# exercise 6
+
+
+
+# exercise 7
+n = int(input("please enter a positive number : "))
+print("sum of numbers from 1 to %i is %i"%(n,(n*(n+1)/2)))
+
+
+# exercise 8
+number_of_widgets = int(input("enter number of widgets"))
+number_of_Gizmos = int(input("enter number of Gizmos"))
+print("total weight of order is : ", number_of_Gizmos*112 + number_of_widgets * 75)
+
+
+# exercise 9
+
+
+
+# exercise 10
+a = int(input("enter first number"))
+b = int(input("enter second number"))
+print("sum of a and b is : ",a+b)
+print("product of a and b is : ",a*b)
+print("difference of a and b is : ",a-b)
+print("division of a and b is : ",a/b)
+print("reminder of a / b is : ",a%b)
+print("result of a^b is : ",a**b)
+
+
+
+
